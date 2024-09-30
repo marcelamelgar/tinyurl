cd5082e659aa086d42e8271841aa67579ed9a4f8
Marcela Melgar <61280126+marcelamelgar@users.noreply.github.com>
2024-09-29 21:15:46 -0600

    primera prueba

---
+++ frontend/scr/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import TinyUrlApp from './components/TinyUrlApp';
+
+function App() {
+    return (
+        <div className="App">
+            <TinyUrlApp />
+        </div>
+    );
+}
+
+export default App;
